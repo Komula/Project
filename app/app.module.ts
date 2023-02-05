952a9dc82492388ad2666285e1a9cda8250a1d05
Komula <123647965+Komula@users.noreply.github.com>
2023-02-04 19:34:02 -0500

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { FooterComponent } from './footer/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopnavComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
